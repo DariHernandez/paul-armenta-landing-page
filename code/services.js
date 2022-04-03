5c530a0b8740fe2cc5748bd4c516547c704d7136
daridev <hernandezdarifrancisco@gmail.com>
2022-04-02 18:11:42 -0600

    ecommerce links

--- code/services.js
+++ code/services.js
@@ -33,3 +33,19 @@ function active_card (card_num) {
     actived_card = services_cards[card_num]
     actived_card.classList.add ("active")
 }
+
+// Read redirect from other pages
+let url_parts = window.location.href.split ("/")
+let url_last_part = url_parts[url_parts.length - 1]
+if (url_last_part.includes("#")) {
+
+    if (url_last_part == "#service-a") {
+        active_card (0)
+    } else if (url_last_part == "#service-b") {
+        active_card (1)
+    } else if (url_last_part == "#service-c") {
+        active_card (2)
+    } else if (url_last_part == "#service-d") {
+        active_card (3)
+    } 
+}
